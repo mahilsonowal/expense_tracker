03ac289d243883d5f436a266c1fcbe8f4fc10f73
Mahil Sonowal <155747136+mahilsonowal@users.noreply.github.com>
2025-09-18 21:16:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace "Expense-Tracker" with your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: "/Expense-Tracker/",
+  base: "/expense_tracker/",
 });
